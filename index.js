478ba78415ac046d93b64c2ba985849e3b390f6d
Dominic Austria <dgaustri@uwaterloo.ca>
2019-08-14 16:10:25 -0500

    add seperate files to remove inline css and js in index.html

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
-app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static("public"))
+
+// app.get('/', function(req, res){
+// 	res.sendFile(__dirname + '/index.html');
+// });
+
 
 io.on('connection', function(socket){
 
-	socket.on('connect'. function(){
-		io.emit('User connected.');
-	});
+	// socket.on('connect', function(){
+	// 	io.emit('User connected.', 0);
+	// });
 
-	socket.on('disconnect', function(){
-		io.emit('User disconnected.');
-	});
+	// socket.on('disconnect', function(){
+	// 	io.emit('User disconnected.');
+	// });
 
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
---
+++ public/page.js
@@ -0,0 +1,22 @@
+$(function () {
+	var socket = io();
+	
+	$('form').submit(function(e){
+		e.preventDefault();
+		socket.emit('chat message', $('#m').val());
+		$('#m').val('');
+		return false;
+	});
+
+	socket.on('chat message', function(msg){
+		$('#messages').append($('<li>').text(msg));
+	});
+
+	// socket.on('user connect', function(){
+	// 	$('#messages').append($('<li>').text('User connected.'));
+	// });
+
+	// socket.on('user disconnect', function(){
+	// 	$('#messages').append($('<li>').text('User disconnected.'));
+	// });
+});
