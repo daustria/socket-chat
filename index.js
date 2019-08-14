cdcefd0107a3305aabc8779fc93a1686ed477149
Dominic Austria <dgaustri@uwaterloo.ca>
2019-08-14 13:59:23 -0500

    implement message sending

--- index.js
+++ index.js
@@ -13,6 +13,10 @@ io.on('connection', function(socket){
 	socket.on('disconnect', function(){
 		console.log('User disconnected.');
 	});
+
+	socket.on('chat message', function(msg){
+		console.log('Message: ' + msg)
+	})
 })
 
 http.listen(3000, function(){
