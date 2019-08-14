16ebbae0119e3d52ee92f30a30d354838976932e
Dominic Austria <dgaustri@uwaterloo.ca>
2019-08-14 13:37:08 -0500

    Add socket-io

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var express = require('express')
 var app = express()
 var http = require('http').createServer(app)
+var io = require('socket.io')(http)
 
 app.get('/', function(req, res){
 	res.sendFile(__dirname + '/index.html')
+})
+
+io.on('connection', function(socket){
+	console.log('User connected.');
+
+	socket.on('disconnect', function(){
+		console.log('User disconnected.');
+	});
 })
 
 http.listen(3000, function(){
