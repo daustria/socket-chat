51d23a9a178bace5c26858dcc6abd83ba318eea6
Dominic Austria <dgaustri@uwaterloo.ca>
2019-08-14 14:45:18 -0500

    broadcast sent messages to all users

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express')
-var app = express()
-var http = require('http').createServer(app)
-var io = require('socket.io')(http)
+var express = require('express');
+var app = express();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var port = process.env.PORT || 3000;
 
 app.get('/', function(req, res){
-	res.sendFile(__dirname + '/index.html')
-})
+	res.sendFile(__dirname + '/index.html');
+});
 
 io.on('connection', function(socket){
 	console.log('User connected.');
@@ -15,10 +16,11 @@ io.on('connection', function(socket){
 	});
 
 	socket.on('chat message', function(msg){
-		console.log('Message: ' + msg)
-	})
-})
+		io.emit('chat message', msg);
+	});
 
-http.listen(3000, function(){
-	console.log('listening on *:3000');
-})
+});
+
+http.listen(port, function(){
+	console.log('listening on *:' + port);
+});
