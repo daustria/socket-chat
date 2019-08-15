d826cb75e59153310d65c9fb580f6db091fc7c4f
Dominic Austria <dgaustri@uwaterloo.ca>
2019-08-15 15:07:56 -0500

    Add section to display connected users

--- index.js
+++ index.js
@@ -2,25 +2,20 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
-var crypto = require('crypto'); // for generating random usernames
 var port = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 
-
-
 io.on('connection', function(socket){
-	var usernameLength = 5;
-	var id = crypto.randomBytes(usernameLength).toString('hex');
-	socket.username = id;
-	io.emit('chat message', socket.username + ' has joined.');
 
-	socket.on('chat message', function(msg){
-		io.emit('chat message', msg);
+	io.emit('add message', socket.id + ' has joined.');
+
+	socket.on('add message', function(msg){
+		io.emit('add message', msg);
 	});
 
 	socket.on('disconnect', function(){
-		io.emit('chat message', socket.username + ' has disconnected.')
+		io.emit('add message', socket.id + ' has disconnected.')
 	})
 
 });
--- public/page.js
+++ public/page.js
@@ -1,15 +1,19 @@
 $(function () {
 	var socket = io();
-    var username = ""
+    var username = '';
 
 	$('form').submit(function(e){
 		e.preventDefault();
-		socket.emit('chat message', username + ':' + $('#m').val());
+		socket.emit('add message', username + ': ' + $('#m').val());
 		$('#m').val('');
 		return false;
 	});
 
-	socket.on('chat message', function(msg){
+    socket.on('connect', function(){
+        username = socket.id;
+    })
+
+	socket.on('add message', function(msg){
 		$('#messages').append($('<li>').text(msg));
 	});
 });
