9599bae333ee8d8031c44339eb28ba8f88f09680
Dominic Austria <dgaustri@uwaterloo.ca>
2019-08-15 10:47:50 -0500

    give users nicknames and display when they connect or disconnect

--- index.js
+++ index.js
@@ -2,28 +2,26 @@ var express = require('express');
 var app = express();
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
+var crypto = require('crypto'); // for generating random usernames
 var port = process.env.PORT || 3000;
 
 app.use(express.static("public"))
 
-// app.get('/', function(req, res){
-// 	res.sendFile(__dirname + '/index.html');
-// });
 
 
 io.on('connection', function(socket){
-
-	// socket.on('connect', function(){
-	// 	io.emit('User connected.', 0);
-	// });
-
-	// socket.on('disconnect', function(){
-	// 	io.emit('User disconnected.');
-	// });
+	var usernameLength = 5;
+	var id = crypto.randomBytes(usernameLength).toString('hex');
+	socket.username = id;
+	io.emit('chat message', socket.username + ' has joined.');
 
 	socket.on('chat message', function(msg){
 		io.emit('chat message', msg);
 	});
+
+	socket.on('disconnect', function(){
+		io.emit('chat message', socket.username + ' has disconnected.')
+	})
 
 });
 
--- public/page.js
+++ public/page.js
@@ -1,9 +1,10 @@
 $(function () {
 	var socket = io();
-	
+    var username = ""
+
 	$('form').submit(function(e){
 		e.preventDefault();
-		socket.emit('chat message', $('#m').val());
+		socket.emit('chat message', username + ':' + $('#m').val());
 		$('#m').val('');
 		return false;
 	});
@@ -11,12 +12,5 @@ $(function () {
 	socket.on('chat message', function(msg){
 		$('#messages').append($('<li>').text(msg));
 	});
-
-	// socket.on('user connect', function(){
-	// 	$('#messages').append($('<li>').text('User connected.'));
-	// });
-
-	// socket.on('user disconnect', function(){
-	// 	$('#messages').append($('<li>').text('User disconnected.'));
-	// });
 });
+
